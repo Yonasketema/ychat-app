5ff2536e69c58e9ce04b19cb17c41bf4109593c5
yonasketema <yonask.kitaw@gmail.com>
2024-04-09 13:03:24 +0300

    add api layer

---
+++ lib/api.ts
@@ -0,0 +1,37 @@
+export const signUp = async (body) => {
+  console.log(body);
+  try {
+    const res = await fetch(
+      new Request("http://localhost:8080/api/v1/users/login", {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" },
+      })
+    );
+    // if (res.ok) {
+    //   const data = await res.json();
+    //   console.log(data);
+    // }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getUser = async () => {
+  try {
+    const res = await fetch(
+      new Request("http://localhost:8080/api/v1/users/", {
+        method: "GET",
+        credentials: "include",
+      })
+    );
+    if (res.ok) {
+      const data = await res.json();
+
+      return data.data;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
