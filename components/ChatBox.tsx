e7fde5c01a41f9ada9b968865e83ff1b28422cec
yonasketema <yonask.kitaw@gmail.com>
2024-04-15 15:45:47 +0300

    style: add auto scroll to chatbox

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -1,12 +1,13 @@
 import { useSocket } from "@/context/socketProvider";
 import { getMessagesApi } from "@/lib/api";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MessageInput from "./MessageInput";
 
 function ChatBox({ selectedUser }) {
   const { authUser, socket } = useSocket();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState();
+  const chatBoxRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
     async function getMessages() {
@@ -15,6 +16,15 @@ function ChatBox({ selectedUser }) {
     }
     getMessages();
   }, [selectedUser]);
+
+  useEffect(() => {
+    if (chatBoxRef.current !== null) {
+      chatBoxRef.current.scrollTo({
+        top: chatBoxRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages, newMessage]);
 
   useEffect(() => {
     if (newMessage?.senderId === selectedUser?.id) {
@@ -30,9 +40,12 @@ function ChatBox({ selectedUser }) {
 
   return (
     <section className="h-full flex flex-col justify-between border">
-      <div className="border w-96 h-80 flex flex-col items-center justify-center overflow-y-auto">
+      <div
+        className="border w-96 h-80 flex flex-col items-center  overflow-y-auto"
+        ref={chatBoxRef}
+      >
         {messages?.length === 0 || !messages ? (
-          <p className="mx-auto text-gray-500">Start Message ....</p>
+          <p className=" text-center pt-24 text-gray-500">Start Message ....</p>
         ) : (
           <div className="flex flex-col justify-end  w-full  px-2 pt-20 space-y-2 bg-white">
             {messages?.map((message) => {
